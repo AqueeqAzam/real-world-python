f11f9c54ba3120434ae7e7c0a4fad7c02c69ceed
AQUEEQ AZAM <84491720+AqueeqAzam@users.noreply.github.com>
2024-02-21 10:25:08 +0530

    Update class-average.py

--- class-average.py
+++ class-average.py
@@ -1,3 +1,4 @@
+#----------------------------simple program to demostrate class average of class---------------------------------
 # initializing phase
 total = 0
 grade_counter = 0
@@ -16,3 +17,47 @@ if average >= 40:
 else:
     print('failed')
 
+# ---------------------------asking from user input------------------------------
+# initialization phase
+total = 0
+grade_counter = 0
+
+grades = int(input('Enter grade, -1 to end:'))
+
+# processing phase
+while grade != -1:
+    total = total + grade
+    grade_counter = grade_counter + 1
+    grade = int(input('Enter grade, -1 to end:'))
+
+# termination phase
+    if grade !=0:
+        average = total / grade_counter
+        print('class average is', average)
+    else:
+        print('no grades were found')
+
+
+# -----------------------break and coutinue statements-----------------------------
+for number in range(100):
+    if number == 11:
+        break
+    print(number, end= ' ')
+
+for number in range(10):
+    if number == 5:
+        continue
+    print(number, end=' ')
+
+# ----------------------AND, OR and NOT statements-----------------------------------    
+gender = 'Femail'
+age = 70
+
+if gender == 'Femail' and age == 55:
+    print('senior femail')
+
+if gender == 'Femail' or age == 26:
+    print('Marred')
+
+if not age == 45:
+    print('she can birth')
